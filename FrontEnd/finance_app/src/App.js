857f77649d6d2bf990c5d74f8643610e2464b3c6
Arturo <alangap87@gmail.com>
2022-02-03 19:05:08 -0600

    Worked on Basic Layout

--- FrontEnd/finance_app/src/App.js
+++ FrontEnd/finance_app/src/App.js
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
+import Sidebar from './components/Sidebar'
 import './index.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Finance App</h1>
-      <nav>
-        <Link to="/dashboard">dashboard</Link> |{" "}
-        <Link to="/settings">settings</Link> |{" "}
-      </nav>
-    </div>
+      <div class="flex border border-red-400">
+
+        {/* Sidebar component */}
+        <div class="hidden md:block flex-none items-center px-5 w-55 border border-red-300">
+          <Sidebar/>
+        </div> 
+
+        {/* Main Content Area */}
+        <div class="grow items-center justify-center p-40 border-4 border-dotted">
+          <p class="text-center">Hello World</p>
+        </div>
+
+      </div>
   );
 }
--- FrontEnd/finance_app/src/pages/dashboard.js
+++ FrontEnd/finance_app/src/pages/dashboard.js
@@ -2,8 +2,6 @@
 // main Dashboard page that will load fundamental company data
 export default function Dashboard() {
   return (
-      <h2 className="text-3xl font-bold underline">
-        Dashboard
-      </h2>
+      <h2>Hello</h2>
   );
 }
